507364b013a8bbe8e383c80ef06f2ae5ec21cd65
Jah <moskovienkor@gmail.com>
2020-07-23 16:34:30 +0300

    2) added second test . . .

--- src/main/java/First.java
+++ src/main/java/First.java
@@ -2,4 +2,8 @@ public class First {
     public static void main(String[] args) {
         System.out.println("This is first test . . .");
     }
+
+    public void method() {
+        System.out.println("This is secont test . . .");
+    }
 }
